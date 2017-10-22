f665525ca17506eb3e38792cf722a419045f372b
asus <415519981@qq.com>
2017-10-22 13:50:05 +0800

    密密麻麻

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/entity/ContactInfo.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.entity;
+
+/**
+ * Created by asus on 2017/10/22.
+ */
+
+public class ContactInfo {
+    public String id ;
+    public String name;
+    public String phone;
+
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/utils/ContactInfoParser.java
@@ -0,0 +1,20 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.utils;
+
+import java.util.Collection;
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.m2theftguard.entity.ContactInfo;
+
+/**
+ * Created by asus on 2017/10/22.
+ */
+
+class ContactInfoParser {
+    public static List<ContactInfo> getSystemContact(ContactSelectActivity contactSelectActivity) {
+        return null;
+    }
+
+    public static Collection<? extends ContactInfo> getSimContacts(ContactSelectActivity contactSelectActivity) {
+        return null;
+    }
+}
