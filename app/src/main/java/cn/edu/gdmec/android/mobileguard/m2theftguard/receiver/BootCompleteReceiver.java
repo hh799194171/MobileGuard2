c8b7e04b6f80af98ef94d14b90eb9d67f4db9bdb
hh799194171 <zyxiaomm@163.com>
2017-10-22 21:17:43 +0800

    10.22tt

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteReceiver.java
@@ -6,15 +6,10 @@ import android.content.Intent;
 
 import cn.edu.gdmec.android.mobileguard.App;
 
-/**
- * Created by asus on 2017/10/22.
- */
-
-public class BootCompleteReceiver extends BroadcastReceiver {
+public class BootCompleteReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
         ((App)(context.getApplicationContext())).correctSIM();
-
     }
 }
