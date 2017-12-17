8a55d3b5df6ba4405cf636daebd8d308dc189178
asus <415519981@qq.com>
2017-12-17 14:38:04 +0800

    wei  12.17. 14.36

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m4appmanager/entity/AppInfo.java
@@ -3,7 +3,7 @@ package cn.edu.gdmec.android.mobileguard.m4appmanager.entity;
 import android.graphics.drawable.Drawable;
 
 /**
- * Created by hh on 2017/11/9.
+ * Created by Administrator on 2017/11/7.
  */
 
 public class AppInfo {
@@ -14,17 +14,20 @@ public class AppInfo {
     public long appSize;
     public boolean isInRoom;
     public boolean isUserApp;
-    public boolean isSelected=false;
+    public boolean isSelected = false;
     public String version;
     public String InstallTime;
     public String signature;
     public String permissions;
+    //新变量
+    public String activityName;
     public String getAppLocation(boolean isInRoom){
-        if (isInRoom){
+        if(isInRoom){
             return "手机内存";
-
-        }else {
-            return "外部存储";
+        }else{
+            return "外部内存";
         }
     }
+    //新加
+    public boolean isLock;
 }
---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m7processmanager/entity/TaskInfo.java
@@ -0,0 +1,16 @@
+package cn.edu.gdmec.android.mobileguard.m7processmanager.entity;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Administrator on 2017/11/21.
+ */
+
+public class TaskInfo {
+    public String appName;
+    public Drawable appIcon;
+    public String packageName;
+    public long appMemory;
+    public boolean isChecked;
+    public boolean isUserApp;
+}
